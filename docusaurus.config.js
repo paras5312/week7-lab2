4cc70a5b48b8b9ec7a1045e062aaf21d1ffe6b4f
Paras Soni <parassoni5618@gmail.com>
2025-03-04 17:45:07 +0200

    implement lab 2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://paras5312.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/week7-lab2/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
